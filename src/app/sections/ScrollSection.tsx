f983676a040f2c5f84bb83db765fd1f5e5122a5e
Nicolas <alvaradonicolas317@email.com>
2025-04-20 21:49:51 -0300

    Refactor ScrollSection: mejora la estructura del componente y agrega claves únicas a los elementos del mapeo.

--- src/app/sections/ScrollSection.tsx
+++ src/app/sections/ScrollSection.tsx
@@ -1,27 +1,23 @@
-
-
 const words = ["impredecible", "cambiante", "extravagante"];
 const duplicatedWords = [...words, ...words];
+
 export const ScrollSection = () => {
   return (
-
-
-
-<section className="infinite-wrapper ">
-  <div className="scroll-tracking">
-    {duplicatedWords.map((word, index) => (
-      <div className="flex items-center">
-        <span className="word tracking-widest">
-          {word.toUpperCase()}
-        </span>
-        {(index !== duplicatedWords.length - 1) && (
-          <span className="separator">
-            •
-          </span>
-        )}
+    <section className="infinite-wrapper">
+      <div className="scroll-tracking">
+        {duplicatedWords.map((word, index) => (
+          <div className="flex items-center" key={index}> {/* Agregando key */}
+            <span className="word tracking-widest">
+              {word.toUpperCase()}
+            </span>
+            {index !== duplicatedWords.length - 1 && (
+              <span className="separator">
+                •
+              </span>
+            )}
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
-</section>
-  )
-}
+    </section>
+  );
+};
